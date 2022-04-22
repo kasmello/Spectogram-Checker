8b02a3b784f2e5572a72360b69955966e919cb96
kasmello <karmel.gandahusada@gmail.com>
2022-04-22 15:07:55 +0800

    added whale image models

--- image_viewer/models.py
+++ image_viewer/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class whale_images(models.Model):
+    url = models.TextField()
+    width = models.IntegerField()
+    height = models.IntegerField()
+    label = models.TextField()
