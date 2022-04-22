cb993cbb1c166c451685ec2e5cc38e30b050bf83
kasmello <karmel.gandahusada@gmail.com>
2022-04-22 13:41:03 +0800

    Test whale statistics page created

---
+++ whale_statistics/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+# Create your models here.
+#pull data from dataset
---
+++ whale_statistics/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def say_hello(request):
+    return render(request, 'whale_statistics.html')
+
+def show_stats(request):
+    context = {'Text':'waow'}
+    return render(request, 'whale_statistics.html', context)
