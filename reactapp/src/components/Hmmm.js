e591cef3c144535f5f740bf6dc9a4f0f2ab4ef50
kasmello <karmel.gandahusada@gmail.com>
2022-05-02 10:43:46 +0800

    added header test app

---
+++ reactapp/src/components/Hmmm.js
@@ -0,0 +1,13 @@
+const Hmmm = () => {
+  function sayDaw() {
+    alert('GAWW')
+  }
+  return (
+    <div className = "button">
+      <button onClick={sayDaw}>PRESS ME</button>
+    </div>
+  )
+
+}
+
+export default Hmmm;
--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// these are components!!!
+// components can be either functions or classes
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
